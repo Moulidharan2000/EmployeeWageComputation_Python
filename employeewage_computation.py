fcc5df62a5c99fe1c1adc7fed4296cc8e234261b
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-06-16 17:03:44 +0530

    [Moulidharan] Added : Calculating Employee Wage Per Day

--- employeewage_computation.py
+++ employeewage_computation.py
@@ -3,11 +3,24 @@ from random import randint
 
 def employee_check(random_num):
     if random_num == 1:
-        print("Employee is Present")
+        return True
+    return False
+
+
+def calculate_wage():
+    wage_per_hr = 20
+    working_hr = 8
+    if employee_check(random_num):
+        wage_per_day = wage_per_hr * working_hr
+        print("Employee Wage Per Day : ", wage_per_day)
     else:
-        print("Employee is Absent")
+        print("Employee Wage Per Day : ", 0)
 
 
 if __name__ == "__main__":
     random_num = randint(0, 1)
-    employee_check(random_num)
+    if employee_check(random_num):
+       print("Employee is Present")
+    else:
+       print("Employee is Absent")
+    calculate_wage()
