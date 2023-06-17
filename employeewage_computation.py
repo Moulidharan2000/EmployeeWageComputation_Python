5c119c0aa2d024015065790d7d2e3f2451174723
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-06-17 19:13:22 +0530

    [Moulidharan] Added : Add Part Time Employee and Wage

--- employeewage_computation.py
+++ employeewage_computation.py
@@ -1,26 +1,25 @@
 from random import randint
 
-
-def employee_check(random_num):
-    if random_num == 1:
-        return True
-    return False
+WAGE_PER_HR = 20
+WORKING_HR = 8
+PART_TIME_HR = 4
 
 
 def calculate_wage():
-    wage_per_hr = 20
-    working_hr = 8
-    if employee_check(random_num):
-        wage_per_day = wage_per_hr * working_hr
-        print("Employee Wage Per Day : ", wage_per_day)
-    else:
-        print("Employee Wage Per Day : ", 0)
+    check = True if random_num == 1 or 2 else False
+    wage_per_day = 0
+    if check:
+        print("Employee is Present")
+        if random_num == 1:
+            wage_per_day = WAGE_PER_HR * WORKING_HR
+        elif random_num == 2:
+            print("Employee is Part Time")
+            wage_per_day = PART_TIME_HR * WAGE_PER_HR
+        else:
+            print("Employee is Absent")
+    print("Employee Wage Per Day : ", wage_per_day)
 
 
 if __name__ == "__main__":
-    random_num = randint(0, 1)
-    if employee_check(random_num):
-       print("Employee is Present")
-    else:
-       print("Employee is Absent")
+    random_num = randint(0, 2)
     calculate_wage()
