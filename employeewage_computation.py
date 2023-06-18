d30d76e37dd2230c358d626edf783a64488847a7
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-06-18 11:10:59 +0530

    [Moulidharan] Added : Using match case statement

--- employeewage_computation.py
+++ employeewage_computation.py
@@ -6,17 +6,18 @@ PART_TIME_HR = 4
 
 
 def calculate_wage():
-    check = True if random_num == 1 or 2 else False
+    check = True if random_num == 1 or random_num == 2 else False
     wage_per_day = 0
     if check:
         print("Employee is Present")
-        if random_num == 1:
-            wage_per_day = WAGE_PER_HR * WORKING_HR
-        elif random_num == 2:
-            print("Employee is Part Time")
-            wage_per_day = PART_TIME_HR * WAGE_PER_HR
-        else:
-            print("Employee is Absent")
+        match random_num:
+            case 1:
+                wage_per_day = WAGE_PER_HR * WORKING_HR
+            case 2:
+                print("Employee is Part Time")
+                wage_per_day = PART_TIME_HR * WAGE_PER_HR
+    else:
+        print("Employee is Absent")
     print("Employee Wage Per Day : ", wage_per_day)
 
 
