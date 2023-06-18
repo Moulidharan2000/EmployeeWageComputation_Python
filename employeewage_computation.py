607b4ca298c066bac87eba056eb2c10116f93c77
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-06-18 11:18:38 +0530

    [Moulidharan] Added: Calculating Employee's Wage for Month

--- employeewage_computation.py
+++ employeewage_computation.py
@@ -3,11 +3,13 @@ from random import randint
 WAGE_PER_HR = 20
 WORKING_HR = 8
 PART_TIME_HR = 4
+WORKING_DAYS = 20
 
 
 def calculate_wage():
     check = True if random_num == 1 or random_num == 2 else False
     wage_per_day = 0
+    monthly_wages = 0
     if check:
         print("Employee is Present")
         match random_num:
@@ -18,7 +20,9 @@ def calculate_wage():
                 wage_per_day = PART_TIME_HR * WAGE_PER_HR
     else:
         print("Employee is Absent")
+    monthly_wages = WORKING_DAYS * wage_per_day
     print("Employee Wage Per Day : ", wage_per_day)
+    print("Employee Wage for Month : ", monthly_wages)
 
 
 if __name__ == "__main__":
