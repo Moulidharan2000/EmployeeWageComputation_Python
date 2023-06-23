25b42254f3c3e0af648b0a43d9e93c901c49c796
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-06-23 15:12:05 +0530

    [Moulidharan] Added : Compute Employee Wage for Multiple Companies

--- employeewage_computation.py
+++ employeewage_computation.py
@@ -3,11 +3,11 @@ from random import randint
 
 class Employee:
 
-    def __init__(self, wage_per_hr, working_days, total_working_hrs, emp_name):
+    def __init__(self, emp_name, wage_per_hr, working_days, total_working_hrs):
+        self.emp_name = emp_name
         self.wage_per_hr = wage_per_hr
         self.working_days = working_days
         self.total_working_hrs = total_working_hrs
-        self.emp_name = emp_name
         self.total_wage = 0
 
     def calculate_wage(self):
@@ -31,12 +31,43 @@ class Company:
         self.emp_dict = {}
 
     def add_emp(self, emp_obj):
-        self.emp_dict.update({emp_obj.emp_name: emp_obj})
-        print(self.emp_dict)
+        self.emp_dict.update({emp_obj.emp_name: emp_obj.total_wage})
+
+    def update_emp(self, emp_obj):
+        if emp_obj.emp_name in self.emp_dict:
+            self.emp_dict.update({emp_obj.emp_name: emp_obj.total_wage})
+
+    def remove_emp(self, emp_name):
+        if emp_name in self.emp_dict:
+            self.emp_dict.pop(emp_name)
+
+    def display(self):
+        keys_list = list(self.emp_dict.keys())
+        values_list = list(self.emp_dict.values())
+        for i in range(len(keys_list)):
+            print(keys_list[i], " : ", values_list[i])
 
 
 if __name__ == '__main__':
-    employee = Employee(20, 20, 100, 'Sathish')
-    employee.calculate_wage()
     company = Company("ABC")
-    company.add_emp(employee)
+    while True:
+        option = int(input("\n1.Add Employee\n2.Update Employee\n3.Delete Employee\n4.Display Details\n0.Exit\nEnter the Option : "))
+        if option == 1 or option == 2:
+            emp_name = input("Enter the Employee Name : ")
+            wage_per_hr = int(input("Enter the Wage per Hour : "))
+            working_days = int(input("Enter the number of working days : "))
+            total_working_hrs = int(input("Enter the total working hours : "))
+            employee = Employee(emp_name, wage_per_hr, working_days, total_working_hrs)
+            employee.calculate_wage()
+            company.add_emp(employee) if option == 1 else company.update_emp(employee)
+            print("Employee Details Added...") if option == 1 else print("Employee Details Updated...")
+        elif option == 3:
+            emp_name = input("Enter the Employee Name : ")
+            company.remove_emp(emp_name)
+            print("Employee Details Deleted...")
+        elif option == 4:
+            print("Employee Details : ")
+            company.display()
+        elif option == 0:
+            print("Exited...")
+            break
